b7b35c833a1a91c7def7556f28fff9f185402b00
Mark Tellez <mark.tellez@icloud.com>
2020-09-05 15:49:42 -0500

    start here

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,33 +1,7 @@
 import React from "react";
-import ListItem from "./list-item";
+
 import "./styles.css";
 
-import Filter from "./filter";
-import List from "./list";
-
-import screencasts from "../data/screencasts.json";
-
 export default function App() {
-  function filterScreencasts(query, screencasts) {
-    return screencasts.filter(
-      (screencast) =>
-        screencast.title.toLowerCase().indexOf(query.toLowerCase()) >
-        -1
-    );
-  }
-  return (
-    <div>
-      <Filter
-        placeholder='Type to filter the screencasts'
-        render={(query) => (
-          <List
-            items={filterScreencasts(query, screencasts)}
-            render={(screencast) => (
-              <ListItem screencast={screencast} />
-            )}
-          />
-        )}
-      />
-    </div>
-  );
+  return <div>Start here</div>;
 }
